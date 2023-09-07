4a2ff312e5fc4751717d21ab1c755be06705eaaa
sltsheryl <sltsheryl@gmail.com>
2023-09-07 23:05:11 +0800

    build: add morgan for improved logging

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,12 @@
 import "dotenv/config";
 import express, { Request, Response, NextFunction } from "express";
 import questionRoutes from "./question_service/routes/questions";
+import morgan from "morgan";
 
 const app = express();
+
+app.use(morgan("dev"));
+
 app.use(express.json());
 
 app.use("/api/questions", questionRoutes);
