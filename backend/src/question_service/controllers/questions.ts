9f6c5661784db2046eae3c8eba04f3979c4cdfb7
see leng <seelengxd@gmail.com>
2023-09-08 18:02:29 +0800

    feat: add api endpoint for creating questions
    - add api endpoint for POST /api/questions with validations
    - no image upload yet

--- backend/src/question_service/controllers/questions.ts
+++ backend/src/question_service/controllers/questions.ts
@@ -1,5 +1,7 @@
 import { RequestHandler } from "express";
 import QuestionModel from "../models/question";
+import { body, matchedData, validationResult } from "express-validator";
+import { complexityEnum } from "../models/question";
 
 export const getQuestions: RequestHandler = async (req, res, next) => {
   try {
@@ -10,4 +12,37 @@ export const getQuestions: RequestHandler = async (req, res, next) => {
   }
 };
 
-// TODO: addQuestion, updateQuestion, deleteQuestion
+export const addQuestion: RequestHandler[] = [
+  body("title").notEmpty().withMessage("title cannot be empty."),
+  body("categories")
+    .optional()
+    .isArray()
+    .withMessage("categories should be an array."),
+  body("complexity")
+    .isIn(complexityEnum)
+    .withMessage(`complexity should be one of ${complexityEnum.join(", ")}.`),
+  body("linkToQuestion")
+    .isURL()
+    .withMessage("linkToQuestion should be a valid URL."),
+  body("questionDescription")
+    .notEmpty()
+    .withMessage("questionDescription should not be empty."),
+  async (req, res) => {
+    if (!validationResult(req).isEmpty()) {
+      res.status(400).json({ errors: validationResult(req).array() });
+      return;
+    }
+
+    const formData = matchedData(req);
+    const lastQuestion = await QuestionModel.find({})
+      .sort({ questionID: -1 })
+      .limit(1);
+    const questionID = lastQuestion[0] ? lastQuestion[0].questionID + 1 : 1;
+    const question = new QuestionModel({ ...formData, questionID });
+    await question.save();
+
+    res.sendStatus(200);
+  },
+];
+
+// TODO: updateQuestion, deleteQuestion
