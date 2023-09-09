1a9a802fac59f6e0a3b4c057ad93b9eb60556b32
chloelim <chloelim22@gmail.com>
2023-09-10 04:53:02 +0800

    [frontend] feat: add pagination for datatable

--- frontend/src/components/tables/DataTable.tsx
+++ frontend/src/components/tables/DataTable.tsx
@@ -2,6 +2,7 @@ import { ArrowDownIcon, ArrowUpIcon } from '@chakra-ui/icons';
 import { Table, Tbody, Td, Th, Thead, Tr, chakra } from '@chakra-ui/react';
 import { useReactTable, getCoreRowModel, getSortedRowModel, type SortingState, type ColumnDef, flexRender } from '@tanstack/react-table';
 import React, { useState } from 'react';
+import DataTablePagination from './DataTablePagination';
 interface DataTableProps<T extends object> {
   tableData: T[]
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -22,44 +23,47 @@ const DataTable = <T extends object>({ tableData, columns }: DataTableProps<T>):
   });
 
   return (
-    <Table>
-      <Thead>
-        {table.getHeaderGroups().map((headerGroup) => (
-          <Tr key={headerGroup.id}>
-            {
-              headerGroup.headers.map((header) => {
-                const sortDirection = header.column.getIsSorted();
-                return (
-                <Th key={header.id} onClick={header.column.getToggleSortingHandler()}>
-                  {
-                    flexRender(
-                      header.column.columnDef.header,
-                      header.getContext()
-                    )
-                  }
-                  <chakra.span paddingLeft={2}>
-                    {sortDirection === 'asc' && <ArrowUpIcon />}
-                    {sortDirection === 'desc' && <ArrowDownIcon />}
-                  </chakra.span>
-                </Th>
-                );
-              })
-            }
-          </Tr>
-        ))}
-      </Thead>
-      <Tbody>
-        {table.getRowModel().rows.map((row) => (
-          <Tr key={row.id}>
-            {row.getVisibleCells().map((cell) => (
-              <Td key={cell.id}>
-                {flexRender(cell.column.columnDef.cell, cell.getContext())}
-              </Td>
-            ))}
-          </Tr>
-        ))}
-      </Tbody>
-    </Table>
+    <>
+      <Table>
+        <Thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <Tr key={headerGroup.id}>
+              {
+                headerGroup.headers.map((header) => {
+                  const sortDirection = header.column.getIsSorted();
+                  return (
+                  <Th key={header.id} onClick={header.column.getToggleSortingHandler()}>
+                    {
+                      flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )
+                    }
+                    <chakra.span paddingLeft={2}>
+                      {sortDirection === 'asc' && <ArrowUpIcon />}
+                      {sortDirection === 'desc' && <ArrowDownIcon />}
+                    </chakra.span>
+                  </Th>
+                  );
+                })
+              }
+            </Tr>
+          ))}
+        </Thead>
+        <Tbody>
+          {table.getRowModel().rows.map((row) => (
+            <Tr key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <Td key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </Td>
+              ))}
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+      <DataTablePagination table={table} />
+    </>
   );
 };
 
--- frontend/src/pages/questions/Questions.tsx
+++ frontend/src/pages/questions/Questions.tsx
@@ -1,31 +1,14 @@
 import { createColumnHelper } from '@tanstack/react-table';
 import DataTable from '../../components/tables/DataTable';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { type QuestionData } from '../../types/questions/questions';
 import QuestionsAPI from '../../api/questions/questions';
-import { Skeleton } from '@chakra-ui/react';
-
-const columnHelper = createColumnHelper<QuestionData>();
-const questionDataColumns = [
-  columnHelper.accessor('questionID', {
-    cell: (id): number => id.getValue(),
-    header: 'Question ID'
-  }),
-  columnHelper.accessor('title', {
-    cell: (title): string => title.getValue(),
-    header: 'Question Title'
-  }),
-  columnHelper.accessor('categories', {
-    cell: (categories): string => categories.getValue().join(', '),
-    header: 'Question Categories'
-  }),
-  columnHelper.accessor('complexity', {
-    cell: (complexity): string => complexity.getValue() as string,
-    header: 'Question Complexity'
-  })
-];
+import { Box, Button, Skeleton } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
+import { ChevronRightIcon } from '@chakra-ui/icons';
 
 const Questions: React.FC = () => {
+  const navigate = useNavigate();
   const [questionList, setQuestionList] = useState<QuestionData[]>();
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
 
@@ -41,10 +24,41 @@ const Questions: React.FC = () => {
     }
   }, []);
 
+  const columnHelper = createColumnHelper<QuestionData>();
+  const questionDataColumns = useMemo(() => ([
+    columnHelper.accessor('questionID', {
+      cell: (id): number => id.getValue(),
+      header: 'Question ID'
+    }),
+    columnHelper.accessor('title', {
+      cell: (title): string => title.getValue(),
+      header: 'Question Title'
+    }),
+    columnHelper.accessor('categories', {
+      cell: (categories): string => categories.getValue().join(', '),
+      header: 'Question Categories'
+    }),
+    columnHelper.accessor('complexity', {
+      cell: (complexity): string => complexity.getValue() as string,
+      header: 'Question Complexity'
+    }),
+    columnHelper.accessor('questionID', {
+      header: '',
+      cell: (cell) => (
+      <Button value={cell.getValue()} onClick={() => { navigate(`/question/${cell.getValue()}`); }
+      }>
+        <ChevronRightIcon />
+      </Button>
+      )
+    })
+  ]), []);
+
   return (
-    <Skeleton isLoaded={isLoaded}>
-      {(questionList !== undefined) && <DataTable columns={questionDataColumns} tableData={questionList} />}
-    </Skeleton>
+      <Box padding={4}>
+        <Skeleton isLoaded={isLoaded}>
+          {(questionList !== undefined) && <DataTable columns={questionDataColumns} tableData={questionList} />}
+        </Skeleton>
+      </Box>
   );
 };
 
