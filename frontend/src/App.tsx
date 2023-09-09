952907d9ffa9f6d00958e9bac42e433f4fbac65b
chloelim <chloelim22@gmail.com>
2023-09-09 20:35:01 +0800

    [frontend]: add navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter } from 'react-router-dom';
-import AppRouter from './pages/routing/AppRouter';
+// import AppRouter from './pages/routing/AppRouter';
+import Navbar from './components/navigation/Navbar';
 
 const App: React.FC = () => {
   return (
     <ChakraProvider>
+      <Navbar />
       <BrowserRouter>
-        <AppRouter />
+        {/* <AppRouter /> */}
       </BrowserRouter>
     </ChakraProvider>
   );
