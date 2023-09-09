0c75f30b8fb689b00d4d20dac3b5b22c041ff541
see leng <seelengxd@gmail.com>
2023-09-09 19:34:06 +0800

    feat: set up api service for adding questions

--- frontend/src/api/questions/questions.ts
+++ frontend/src/api/questions/questions.ts
@@ -1,2 +1,18 @@
-// temporary so that app can compile, to remove once file is not empty
-export {};
+import { type QuestionPostData } from '../../types/questions/questions';
+import client from '../base';
+
+export default class QuestionsAPI {
+  protected getQuestionsUrl (): string {
+    return '/questions';
+  }
+
+  public async getCategories (): Promise<string[]> {
+    const response = await client.get(`${this.getQuestionsUrl()}/categories`);
+    const categories = response.data.data as string[];
+    return categories;
+  }
+
+  public async addQuestion (question: QuestionPostData): Promise<never> {
+    return await client.post(this.getQuestionsUrl(), question);
+  }
+}
