3f796c2c7092ab49fae43e70a462c00dd9e6fc63
see leng <seelengxd@gmail.com>
2023-09-11 10:12:41 +0800

    refactor: extract autocomplete to separate component

---
+++ frontend/src/components/questions/QuestionCategoryAutocomplete.tsx
@@ -0,0 +1,65 @@
+import {
+  AutoComplete,
+  AutoCompleteInput,
+  AutoCompleteItem,
+  AutoCompleteList,
+  AutoCompleteTag,
+} from '@choc-ui/chakra-autocomplete';
+import React, { useEffect, useState } from 'react';
+import QuestionsAPI from '../../api/questions/questions';
+
+interface QuestionCategoryAutocompleteProps {
+  categories: string[];
+  handleChange: (categories: string[]) => void;
+  showTags: boolean;
+}
+
+const QuestionCategoryAutocomplete = ({
+  categories,
+  handleChange,
+  showTags,
+}: QuestionCategoryAutocompleteProps): JSX.Element => {
+  const [allCategories, setAllCategories] = useState<string[]>([]);
+  useEffect(() => {
+    new QuestionsAPI()
+      .getCategories()
+      .then((categories) => {
+        setAllCategories(categories);
+      })
+      .catch(console.error);
+  }, []);
+
+  return (
+    <AutoComplete
+      openOnFocus
+      closeOnSelect
+      closeOnBlur
+      multiple
+      value={categories}
+      onChange={(categories) => {
+        handleChange(categories as string[]);
+      }}
+      isLoading={allCategories.length === 0}
+    >
+      <AutoCompleteInput variant="filled">
+        {showTags &&
+          (({ tags }) =>
+            tags.map((tag, tid) => <AutoCompleteTag key={tid} label={tag.label as string} onRemove={tag.onRemove} />))}
+      </AutoCompleteInput>
+      <AutoCompleteList>
+        {allCategories.map((category, cid) => (
+          <AutoCompleteItem
+            key={`option-${cid}`}
+            value={category}
+            _selected={{ bg: 'whiteAlpha.50' }}
+            _focus={{ bg: 'whiteAlpha.100' }}
+          >
+            {category}
+          </AutoCompleteItem>
+        ))}
+      </AutoCompleteList>
+    </AutoComplete>
+  );
+};
+
+export default QuestionCategoryAutocomplete;
--- frontend/src/pages/questions/CreateQuestion.tsx
+++ frontend/src/pages/questions/CreateQuestion.tsx
@@ -12,18 +12,12 @@ import {
   Textarea,
   useToast,
 } from '@chakra-ui/react';
-import {
-  AutoComplete,
-  AutoCompleteInput,
-  AutoCompleteItem,
-  AutoCompleteList,
-  AutoCompleteTag,
-} from '@choc-ui/chakra-autocomplete';
 
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import QuestionsAPI from '../../api/questions/questions';
 import { useNavigate } from 'react-router-dom';
 import { type AxiosError } from 'axios';
+import QuestionCategoryAutocomplete from '../../components/questions/QuestionCategoryAutocomplete';
 
 export const CreateQuestion: React.FC = () => {
   const [title, setTitle] = useState('');
@@ -31,22 +25,12 @@ export const CreateQuestion: React.FC = () => {
   const [categories, setCategories] = useState<string[]>([]);
   const [complexity, setComplexity] = useState('Easy');
   const [linkToQuestion, setLinkToQuestion] = useState('');
-  const [allCategories, setAllCategories] = useState<string[]>([]);
 
   const toast = useToast();
 
   const linkPrefix = 'https://leetcode.com/problems/';
 
   const navigate = useNavigate();
-
-  useEffect(() => {
-    new QuestionsAPI()
-      .getCategories()
-      .then((categories) => {
-        setAllCategories(categories);
-      })
-      .catch(console.error);
-  }, []);
 
   const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
@@ -102,36 +86,7 @@ export const CreateQuestion: React.FC = () => {
           </FormControl>
           <FormControl isRequired>
             <FormLabel>Categories</FormLabel>
-            <AutoComplete
-              openOnFocus
-              closeOnSelect
-              closeOnBlur
-              multiple
-              onChange={(categories) => {
-                setCategories(categories as string[]);
-              }}
-              isLoading={allCategories.length === 0}
-            >
-              <AutoCompleteInput variant="filled">
-                {({ tags }) =>
-                  tags.map((tag, tid) => (
-                    <AutoCompleteTag key={tid} label={tag.label as string} onRemove={tag.onRemove} />
-                  ))
-                }
-              </AutoCompleteInput>
-              <AutoCompleteList>
-                {allCategories.map((category, cid) => (
-                  <AutoCompleteItem
-                    key={`option-${cid}`}
-                    value={category}
-                    _selected={{ bg: 'whiteAlpha.50' }}
-                    _focus={{ bg: 'whiteAlpha.100' }}
-                  >
-                    {category}
-                  </AutoCompleteItem>
-                ))}
-              </AutoCompleteList>
-            </AutoComplete>
+            <QuestionCategoryAutocomplete categories={categories} handleChange={setCategories} showTags={true} />
           </FormControl>
           <FormControl isRequired>
             <FormLabel>Complexity</FormLabel>
