df816a25bc1390f64343b11b531dec45c46ddae1
chloelim <chloelim22@gmail.com>
2023-09-15 14:31:26 +0800

    [frontend] feat: code editor lang selector

--- frontend/src/components/code/CodeEditor.tsx
+++ frontend/src/components/code/CodeEditor.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Editor from '@monaco-editor/react';
+import { Select } from '@chakra-ui/react';
+import { EditorLanguageOptions } from '../../types/code/languages';
 
 interface CodeEditorProps {
   theme?: string;
 }
 
 const CodeEditor: React.FC<CodeEditorProps> = ({ theme }: CodeEditorProps) => {
+  const [selectedLanguage, setSelectedLangugage] = useState('javascript');
   return (
-    <Editor height="85vh" width="100%" theme={theme} defaultLanguage="javascript" defaultValue="// some comment" />
+    <>
+      <Select
+        value={selectedLanguage}
+        onChange={(e) => {
+          setSelectedLangugage(e.target.value);
+        }}
+      >
+        {EditorLanguageOptions.map((option, index) => (
+          <option key={index} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </Select>
+      <Editor height="85vh" width="100%" theme={theme} language={selectedLanguage} />
+    </>
   );
 };
 
---
+++ frontend/src/types/code/languages.ts
@@ -0,0 +1,146 @@
+export const EditorLanguageOptions = Object.freeze([
+  {
+    label: 'JavaScript',
+    value: 'javascript',
+  },
+  {
+    label: 'TypeScript',
+    value: 'typescript',
+  },
+  {
+    label: 'Python',
+    value: 'python',
+  },
+  {
+    label: 'Java',
+    value: 'java',
+  },
+  {
+    label: 'C',
+    value: 'c',
+  },
+  {
+    label: 'C++',
+    value: 'cpp',
+  },
+  {
+    label: 'Batch Script',
+    value: 'bat',
+  },
+  {
+    label: 'Clojure',
+    value: 'clojure',
+  },
+  {
+    label: 'CSS',
+    value: 'css',
+  },
+  {
+    label: 'C#',
+    value: 'csharp',
+  },
+  {
+    label: 'Dart',
+    value: 'dart',
+  },
+  {
+    label: 'Dockerfile',
+    value: 'dockerfile',
+  },
+  {
+    label: 'Elixir',
+    value: 'elixir',
+  },
+  {
+    label: 'F#',
+    value: 'fsharp',
+  },
+  {
+    label: 'Go',
+    value: 'go',
+  },
+  {
+    label: 'Graphql',
+    value: 'graphql',
+  },
+  {
+    label: 'JSON',
+    value: 'json',
+  },
+  {
+    label: 'Julia',
+    value: 'julia',
+  },
+  {
+    label: 'Kotlin',
+    value: 'kotlin',
+  },
+  {
+    label: 'Lua',
+    value: 'lua',
+  },
+  {
+    label: 'Markdown',
+    value: 'markdown',
+  },
+  {
+    label: 'MySQL',
+    value: 'mysql',
+  },
+  {
+    label: 'Objective-C',
+    value: 'objective-c',
+  },
+  {
+    label: 'Pascal',
+    value: 'pascal',
+  },
+  {
+    label: 'Perl',
+    value: 'perl',
+  },
+  {
+    label: 'PHP',
+    value: 'php',
+  },
+  {
+    label: 'Plain Text',
+    value: 'text',
+  },
+  {
+    label: 'PostgreSQL',
+    value: 'pgsql',
+  },
+  {
+    label: 'Power Query',
+    value: 'powerquery',
+  },
+  {
+    label: 'PowerShell',
+    value: 'powershell',
+  },
+  {
+    label: 'R',
+    value: 'r',
+  },
+  {
+    label: 'Ruby',
+    value: 'ruby',
+  },
+  {
+    label: 'Rust',
+    value: 'rust',
+  },
+  {
+    label: 'Scala',
+    value: 'scala',
+  },
+  {
+    label: 'SQL',
+    value: 'sql',
+  },
+  {
+    label: 'Swift',
+    value: 'swift',
+  },
+]);
