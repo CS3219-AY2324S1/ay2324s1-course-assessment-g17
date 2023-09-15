0e1f04d7beb6b4e9b739909f25e2fbb4d4adfaa9
sltsheryl <sltsheryl@gmail.com>
2023-09-16 04:10:34 +0800

    fix: add email field to user table and unique constraints

--- backend/user-service/src/models/prisma/seed.ts
+++ backend/user-service/src/models/prisma/seed.ts
@@ -1,66 +1,58 @@
-import { User, UserLanguage, Language } from "@prisma/client";
 import prisma from "../../lib/prisma";
 
-async function seed(): Promise<void> {
-  const users = [
-    {
-      username: "admin_user",
-      password: "admin_password",
-      role: "ADMIN",
-    },
-    {
-      username: "regular_user",
-      password: "user_password",
-      role: "USER",
-    },
-  ];
-
-  const programmingLanguages = [
-    { language: "Java" },
-    { language: "Python" },
-    { language: "C++" },
-  ];
-
-  const userLanguages = [
-    {
-      user_id: 1,
-      language_id: 2,
-    },
-    {
-      user_id: 2,
-      language_id: 1,
-    },
-  ];
-
-  for (const userData of users) {
-    await prisma.user.create({
-      data: userData as User,
-    });
-  }
-
-  for (const languageData of programmingLanguages) {
-    await prisma.language.create({
-      data: languageData as Language,
-    });
-  }
-
-  for (const userLanguageData of userLanguages) {
-    await prisma.userLanguage.create({
-      data: userLanguageData as UserLanguage,
-    });
-  }
-}
-
-async function main() {
+async function seed() {
   try {
-    console.log("creating default database...");
-    await seed();
-    console.log("database seeded");
+    // Seed the User and Language tables
+    const user1 = await prisma.user.create({
+      data: {
+        username: "admin_user",
+        email: "admin@example.com",
+        password: "admin_password",
+        role: "ADMIN",
+      },
+    });
+
+    const user2 = await prisma.user.create({
+      data: {
+        username: "regular_user",
+        email: "user@example.com",
+        password: "user_password",
+        role: "USER",
+      },
+    });
+
+    const javaLanguage = await prisma.language.create({
+      data: { language: "Java" },
+    });
+
+    const pythonLanguage = await prisma.language.create({
+      data: { language: "Python" },
+    });
+
+    const cPlusPlusLanguage = await prisma.language.create({
+      data: { language: "C++" },
+    });
+
+    await prisma.userLanguage.create({
+      data: {
+        user_id: user1.id,
+        language_id: pythonLanguage.id, 
+      },
+    });
+
+    await prisma.userLanguage.create({
+      data: {
+        user_id: user2.id,
+        language_id: javaLanguage.id, 
+      },
+    });
+
+    console.log("Database seeded successfully.");
   } catch (error) {
-    console.error("error with db", error);
+    console.error("Error seeding database:", error);
   } finally {
     await prisma.$disconnect();
   }
 }
 
-main();
+seed();
