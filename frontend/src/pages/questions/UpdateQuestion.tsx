35f515f8c7b586ba52814ca97195bf2a6e816118
see leng <seelengxd@gmail.com>
2023-09-15 16:44:52 +0800

    refactor: move form state into the form element

--- frontend/src/pages/questions/UpdateQuestion.tsx
+++ frontend/src/pages/questions/UpdateQuestion.tsx
@@ -1,11 +1,8 @@
-import { Spinner, Stack, useToast, Card } from '@chakra-ui/react';
 import React, { useEffect, useState } from 'react';
 import QuestionsAPI from '../../api/questions/questions';
-import { useNavigate, useParams } from 'react-router-dom';
-import { type AxiosError } from 'axios';
-import { BiSolidBookAdd } from 'react-icons/bi';
-import IconWithText from '../../components/content/IconWithText';
+import { useParams } from 'react-router-dom';
 import QuestionForm from '../../components/content/QuestionForm';
+import { LINK_PREFIX, type QuestionPostData, type QuestionData } from '../../types/questions/questions';
 
 export const UpdateQuestion: React.FC = () => {
   const { questionId } = useParams();
@@ -15,38 +12,15 @@ export const UpdateQuestion: React.FC = () => {
   } else {
     throw new Error('ID of question is undefined');
   }
-  const [title, setTitle] = useState('');
-  const [questionDescription, setQuestionDescription] = useState('');
-  const [categories, setCategories] = useState<string[]>([]);
-  const [complexity, setComplexity] = useState('Easy');
-  const [linkToQuestion, setLinkToQuestion] = useState('');
-  const [allCategories, setAllCategories] = useState<string[]>([]);
+
   const [dataLoaded, setDataLoaded] = useState(false); // Add this state
-
-  const toast = useToast();
-
-  const linkPrefix = 'https://leetcode.com/problems/';
-
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    new QuestionsAPI()
-      .getCategories()
-      .then((categories) => {
-        setAllCategories(categories);
-      })
-      .catch(console.error);
-  }, []);
+  const [questionData, setQuestionData] = useState<QuestionData | null>(null);
 
   useEffect(() => {
     new QuestionsAPI()
       .getQuestion(questionIdString)
       .then((questionData) => {
-        setTitle(questionData.title);
-        setQuestionDescription(questionData.questionDescription);
-        setCategories(questionData.categories);
-        setComplexity(questionData.complexity);
-        setLinkToQuestion(questionData.linkToQuestion.replace(linkPrefix, ''));
+        setQuestionData({ ...questionData, linkToQuestion: questionData.linkToQuestion.replace(LINK_PREFIX, '') });
         setDataLoaded(true); // Mark data as loaded
       })
       .catch((error) => {
@@ -55,73 +29,18 @@ export const UpdateQuestion: React.FC = () => {
       });
   }, []);
 
-  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
-    e.preventDefault();
-
-    if (categories.length === 0) {
-      toast({
-        title: 'Missing Categories!',
-        description: 'Please fill in the question categories before submitting.',
-        status: 'error',
-        duration: 9000,
-        isClosable: true,
-      });
-      return;
-    }
-
-    new QuestionsAPI()
-      .updateQuestion(questionIdString, {
-        title,
-        questionDescription,
-        categories,
-        complexity,
-        linkToQuestion: linkPrefix + linkToQuestion,
-      })
-      .then(() => {
-        navigate('/');
-      })
-      .catch((err: AxiosError<{ errors: Array<{ msg: string }> }>) => {
-        const errors = err?.response?.data?.errors;
-        if (errors !== undefined) {
-          errors.map((error) =>
-            toast({
-              title: 'Question update failed.',
-              description: error.msg,
-              status: 'error',
-              duration: 9000,
-              isClosable: true,
-            }),
-          );
-        }
-      });
-  };
+  const handleData = async (questionData: QuestionPostData): Promise<QuestionData> =>
+    await new QuestionsAPI().updateQuestion(questionIdString, questionData);
 
   return (
-    <Card m={12} p={8}>
-      <form onSubmit={handleSubmit}>
-        <Stack spacing={4}>
-          <IconWithText text="Update Question" icon={<BiSolidBookAdd size={25} />} fontSize={'2xl'} fontWeight="bold" />
-          {dataLoaded ? (
-            <QuestionForm
-              title={title}
-              setTitle={setTitle}
-              questionDescription={questionDescription}
-              setQuestionDescription={setQuestionDescription}
-              categories={categories}
-              setCategories={setCategories}
-              complexity={complexity}
-              setComplexity={setComplexity}
-              linkToQuestion={linkToQuestion}
-              setLinkToQuestion={setLinkToQuestion}
-              allCategories={allCategories}
-              dialogBody={'Are you sure? Any progress on the form will not be saved. This action is irreversible!'}
-              dialogHeader={'Cancel Question Update'}
-            />
-          ) : (
-            <Spinner size="xl" />
-          )}
-        </Stack>
-      </form>
-    </Card>
+    <QuestionForm
+      formTitle={'Update Question'}
+      dialogBody={'Are you sure? Any progress on the form will not be saved. This action is irreversible!'}
+      dialogHeader={'Cancel Question Update'}
+      handleData={handleData}
+      isLoading={!dataLoaded}
+      initialData={questionData}
+      errorTitle={'Question update failed.'}
+    />
   );
 };
