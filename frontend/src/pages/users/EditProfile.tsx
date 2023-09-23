5b27bd2761bc33c740a0af2214c13e128ff46d9e
Carrie Zheng Jiarui <carriezhengjiarui@gmail.com>
2023-09-23 12:26:40 +0800

    Add languages dropdown list

--- frontend/src/pages/users/EditProfile.tsx
+++ frontend/src/pages/users/EditProfile.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Modal,
   ModalOverlay,
@@ -11,8 +11,17 @@ import {
   Input,
   FormControl,
   FormLabel,
-  Stack, // Add Stack for spacing between fields
+  Stack,
+  useColorModeValue,
 } from '@chakra-ui/react';
+import {
+  AutoComplete,
+  AutoCompleteInput,
+  AutoCompleteItem,
+  AutoCompleteList,
+  AutoCompleteTag,
+} from '@choc-ui/chakra-autocomplete';
+import { EditorLanguageEnum } from '../../types/code/languages';
 
 interface EditProfileProps {
   isOpen: boolean;
@@ -20,6 +29,12 @@ interface EditProfileProps {
 }
 
 const EditProfile: React.FC<EditProfileProps> = ({ isOpen, onCloseModal }) => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [role, setRole] = useState('');
+  const [languages, setLanguages] = useState<string[]>([]);
+  const allLanguages = Object.values(EditorLanguageEnum);
+
   return (
     <Modal isOpen={isOpen} onClose={onCloseModal}>
       <ModalOverlay />
@@ -27,27 +42,85 @@ const EditProfile: React.FC<EditProfileProps> = ({ isOpen, onCloseModal }) => {
         <ModalHeader>Edit Profile</ModalHeader>
         <ModalCloseButton />
         <ModalBody>
+          {/* <form onSubmit={handleSubmit}> */}
           <Stack spacing={4}>
             <FormControl>
               <FormLabel>Username</FormLabel>
-              <Input type="text" placeholder="Username" />
+              <Input
+                type="text"
+                value={username}
+                onChange={(e) => {
+                  setUsername(e.target.value);
+                }}
+              />
             </FormControl>
             <FormControl>
               <FormLabel>Email</FormLabel>
-              <Input type="text" placeholder="Email" />
+              <Input
+                type="text"
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
             </FormControl>
             <FormControl>
               <FormLabel>Role</FormLabel>
-              <Input type="text" placeholder="Role" />
+              <Input
+                type="text"
+                value={role}
+                onChange={(e) => {
+                  setRole(e.target.value);
+                }}
+              />
             </FormControl>
             <FormControl>
               <FormLabel>Languages</FormLabel>
-              <Input type="text" placeholder="Languages" />
+              <AutoComplete
+                openOnFocus
+                closeOnSelect
+                multiple
+                onChange={(selectedLanguages) => {
+                  setLanguages(selectedLanguages as string[]);
+                }}
+                isLoading={allLanguages.length === 0}
+                suggestWhenEmpty
+                restoreOnBlurIfEmpty={false}
+                value={languages}
+                placement="bottom-start"
+              >
+                <AutoCompleteInput variant="filled" isRequired={false}>
+                  {({ tags }) =>
+                    tags.map((tag, tid) => (
+                      <AutoCompleteTag key={tid} label={tag.label as string} onRemove={tag.onRemove} />
+                    ))
+                  }
+                </AutoCompleteInput>
+                <AutoCompleteList
+                  _before={{
+                    top: 'auto',
+                    bottom: 0,
+                  }}
+                >
+                  {allLanguages.map((language, lid) => (
+                    <AutoCompleteItem
+                      key={`option-${lid}`}
+                      value={language}
+                      style={{ marginTop: 4, marginBottom: 4 }}
+                      _selected={{ bg: useColorModeValue('blackAlpha.50', 'whiteAlpha.50'), color: 'gray.500' }}
+                      _focus={{ bg: useColorModeValue('blackAlpha.100', 'whiteAlpha.100') }}
+                    >
+                      {language}
+                    </AutoCompleteItem>
+                  ))}
+                </AutoCompleteList>
+              </AutoComplete>
             </FormControl>
           </Stack>
+          {/* </form> */}
         </ModalBody>
         <ModalFooter>
-          <Button variant="solid" colorScheme="teal" mr={4}>
+          <Button colorScheme="teal" mr={4}>
             Save
           </Button>
         </ModalFooter>
