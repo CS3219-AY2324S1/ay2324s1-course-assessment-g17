69f0521b591c6ebf93acb7a46a5ed096d53bbd08
see leng <seelengxd@gmail.com>
2023-09-30 14:18:11 +0800

    feat: send socket match request details

--- backend/matching-service/src/socket/matchingHandler.ts
+++ backend/matching-service/src/socket/matchingHandler.ts
@@ -2,6 +2,21 @@ import http from "http";
 import dotenv from "dotenv";
 import { Server, Socket } from "socket.io";
 
-const registerMatchingHandlers = (io: Server, socket: Socket) => {};
+export enum QuestionComplexityEnum {
+  EASY = "Easy",
+  MEDIUM = "Medium",
+  HARD = "Hard",
+}
+
+interface RequestMatchProps {
+  complexity: QuestionComplexityEnum;
+  categories: string[];
+}
+
+const registerMatchingHandlers = (io: Server, socket: Socket) => {
+  socket.on("requestMatch", (props: RequestMatchProps) => {
+    console.log(props);
+  });
+};
 
 export default registerMatchingHandlers;
--- frontend/src/pages/matching/Matching.tsx
+++ frontend/src/pages/matching/Matching.tsx
@@ -1,31 +1,72 @@
-import React, { useEffect } from 'react';
-import { Card, Text } from '@chakra-ui/react';
-import { BiSad } from 'react-icons/bi';
-import { io } from 'socket.io-client';
+import React, { useEffect, useState } from 'react';
+import { Button, Card, Center, FormControl, FormLabel, HStack, Select, Stack } from '@chakra-ui/react';
+import { io, type Socket } from 'socket.io-client';
+import QuestionCategoryAutocomplete from '../../components/questions/QuestionCategoryAutocomplete';
+import { QuestionComplexityEnum } from '../../types/questions/questions';
+import { FaBoltLightning } from 'react-icons/fa6';
 import IconWithText from '../../components/content/IconWithText';
 
 const Matching: React.FC = () => {
-  useEffect(() => {
-    const socket = io('http://localhost:9000');
+  const [socket, setSocket] = useState<Socket | null>(null);
+  const [complexity, setComplexity] = useState(QuestionComplexityEnum.EASY);
+  const [categories, setCategories] = useState<string[]>([]);
 
-    socket.on('connect', () => {
+  useEffect(() => {
+    const newSocket = io('http://localhost:9000');
+
+    newSocket.on('connect', () => {
       console.log('Connected to server');
     });
 
-    socket.on('disconnect', () => {
+    newSocket.on('disconnect', () => {
       console.log('Disconnected from server');
     });
 
+    setSocket(newSocket);
+
     return () => {
-      socket.disconnect();
+      newSocket.disconnect();
     };
   }, []);
 
+  const handleMatchRequest: React.FormEventHandler<HTMLFormElement> = (e): void => {
+    e.preventDefault();
+    socket?.emit('requestMatch', { complexity, categories });
+  };
+
   return (
-    <Card m={12} p={8}>
-      <IconWithText text={'Not implemented yet!'} icon={<BiSad />} fontSize={'2xl'} fontWeight="bold" />
-      <Text>Look at console: You should see &quot;Connected to server.&quot;</Text>
-    </Card>
+    <Center width={'100%'}>
+      <Card m={12} p={8} minWidth={'50%'}>
+        <IconWithText text={'Request Match'} icon={<FaBoltLightning size={25} />} fontSize={'2xl'} fontWeight="bold" />
+        <form onSubmit={handleMatchRequest}>
+          <Stack spacing={4}>
+            <HStack mt={2}>
+              <FormControl isRequired>
+                <FormLabel>Complexity</FormLabel>
+                <Select
+                  value={complexity}
+                  onChange={(e) => {
+                    setComplexity(e.target.value as QuestionComplexityEnum);
+                  }}
+                >
+                  <option value={QuestionComplexityEnum.EASY}>Easy</option>
+                  <option value={QuestionComplexityEnum.MEDIUM}>Medium</option>
+                  <option value={QuestionComplexityEnum.HARD}>Hard</option>
+                </Select>
+              </FormControl>
+            </HStack>
+
+            <FormControl isRequired>
+              <FormLabel>Categories</FormLabel>
+              <QuestionCategoryAutocomplete categories={categories} handleChange={setCategories} />
+            </FormControl>
+            <Button type="submit" colorScheme="teal" leftIcon={<FaBoltLightning size={20} />}>
+              Find a match!
+            </Button>
+          </Stack>
+        </form>
+      </Card>{' '}
+    </Center>
   );
 };
 
