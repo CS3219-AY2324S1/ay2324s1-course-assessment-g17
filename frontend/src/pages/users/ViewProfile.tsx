f300b8a6c50b88b31a31a8bbf366249574d28c62
Carrie Zheng Jiarui <carriezhengjiarui@gmail.com>
2023-10-02 16:49:47 +0800

    Remove unnecessary useEffect

--- frontend/src/pages/users/ViewProfile.tsx
+++ frontend/src/pages/users/ViewProfile.tsx
@@ -1,32 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Box, Text, useColorModeValue, HStack } from '@chakra-ui/react';
 import { EmailIcon } from '@chakra-ui/icons';
 import { FaUserGroup, FaCode } from 'react-icons/fa6';
-import AuthAPI from '../../api/users/auth';
-import type { Language } from '../../types/users/users';
+import { useAppSelector } from '../../reducers/hooks';
+import { selectUser } from '../../reducers/authSlice';
 
 const ViewProfile: React.FC = () => {
-  const [userData, setUserData] = useState({
-    username: '',
-    email: '',
-    role: '',
-    languages: [] as Language[],
-  });
-
-  useEffect(() => {
-    const fetchUserProfile = async (): Promise<void> => {
-      try {
-        const response = await new AuthAPI().getCurrentUser();
-        setUserData(response);
-      } catch (error) {
-        console.error('Error fetching user profile:', error);
-      }
-    };
-
-    fetchUserProfile().catch((error) => {
-      console.error('Error fetching user profile:', error);
-    });
-  }, []);
+  const user = useAppSelector(selectUser);
 
   return (
     <Box width="100%" height="85vh" my={5}>
@@ -40,28 +20,28 @@ const ViewProfile: React.FC = () => {
         mx="auto"
       >
         <Text fontSize="xl" fontWeight="bold" textAlign="center">
-          {userData.username}
+          {user?.username}
         </Text>
         <Box textAlign="left" pl={12} pr={12} pt={2} pb={8}>
           <HStack spacing={5} align="center" mt={4}>
             <EmailIcon boxSize={6} />
             <Text>
               <span style={{ fontWeight: 'bold' }}>Email: </span>
-              {userData.email}
+              {user?.email}
             </Text>
           </HStack>
           <HStack spacing={5} align="center" mt={4}>
             <FaUserGroup fontSize="24px" />
             <Text>
               <span style={{ fontWeight: 'bold' }}>Role: </span>
-              {userData.role}
+              {user?.role}
             </Text>
           </HStack>
           <HStack spacing={5} align="center" mt={4}>
             <FaCode fontSize="24px" />
             <Text>
               <span style={{ fontWeight: 'bold' }}>Languages: </span>
-              {userData.languages.length > 0 ? userData.languages.map((lang) => lang.language).join(', ') : 'None'}
+              {(user?.languages?.length ?? 0) > 0 ? user?.languages?.map((lang) => lang.language).join(', ') : 'None'}
             </Text>
           </HStack>
         </Box>
