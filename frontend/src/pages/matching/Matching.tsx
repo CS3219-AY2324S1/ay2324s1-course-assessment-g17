f8ccbc9f6bc88ab1f070450d4a46db1317beb2d8
Justin Lam, juslam19 <e0552250@u.nus.edu>
2023-10-11 21:03:22 +0800

    feat: frontend timer and cancel,  backend disconnect function for matching service

--- frontend/src/pages/matching/Matching.tsx
+++ frontend/src/pages/matching/Matching.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { Card, Center, Spinner, Text, useToast } from '@chakra-ui/react';
+import { Card, Center, Stack, Button, Text, useToast } from '@chakra-ui/react';
 import { io, type Socket } from 'socket.io-client';
 import type { QuestionComplexityEnum } from '../../types/questions/questions';
-import { FaBoltLightning } from 'react-icons/fa6';
+import { FaBoltLightning, FaExclamation } from 'react-icons/fa6';
 import IconWithText from '../../components/content/IconWithText';
 import MatchingForm from './MatchingForm';
+import CountdownProgressBar from './CountdownProgressBar';
 import { useAppSelector } from '../../reducers/hooks';
 import { selectUser } from '../../reducers/authSlice';
 import { useNavigate } from 'react-router-dom';
@@ -68,15 +69,29 @@ const Matching: React.FC = () => {
     setMatchingState(MatchingStateEnum.PENDING);
   };
 
+  const handleCancel: React.FormEventHandler<HTMLFormElement> = (e) => {
+    socket?.emit('disconnect');
+    setMatchingState(MatchingStateEnum.NO_REQUEST);
+  };
+
   return (
     <Center width={'100%'}>
       <Card m={12} p={8} minWidth={'50%'}>
         <IconWithText text={'Request Match'} icon={<FaBoltLightning size={25} />} fontSize={'2xl'} fontWeight="bold" />
         {matchingState === MatchingStateEnum.NO_REQUEST && <MatchingForm handleMatchRequest={handleMatchRequest} />}
         {matchingState === MatchingStateEnum.PENDING && (
-          <Center>
-            <Spinner size={'xl'} />
-          </Center>
+          <Stack>
+            <Center width={'100%'}>
+              <CountdownProgressBar duration={30} onComplete={() => {}} />
+            </Center>
+            <Center width={'100%'}>
+              <form onSubmit={handleCancel}>
+                <Button type="submit" colorScheme="teal" leftIcon={<FaExclamation size={20} />}>
+                  Cancel matching
+                </Button>
+              </form>
+            </Center>
+          </Stack>
         )}
         {matchingState === MatchingStateEnum.MATCHED && <Text>You have been matched!</Text>}
       </Card>
