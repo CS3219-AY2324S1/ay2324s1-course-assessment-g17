7025f9cf88a37a02264547d5bafa53239cec089d
see leng <seelengxd@gmail.com>
2023-10-15 16:57:36 +0800

    feat: set question on frontend

---
+++ frontend/src/pages/collaboration/CollaborationQuestion.tsx
@@ -0,0 +1,64 @@
+import { Box, HStack, VStack, Text, NumberInput, NumberInputField, Button } from '@chakra-ui/react';
+
+import React, { useContext, useEffect, useState } from 'react';
+import QuestionDetails from '../questions/QuestionDetails';
+import { SocketContext } from '../../context/socket';
+import { useParams } from 'react-router-dom';
+
+const CollaborationQuestion: React.FC = () => {
+  const [questionIdInput, setQuestionIdInput] = useState<number | undefined>(undefined);
+  const [questionId, setQuestionId] = useState<number | undefined>(undefined);
+  const { socket } = useContext(SocketContext);
+  const { roomId } = useParams();
+
+  useEffect(() => {
+    socket?.on('set-question', (questionId: number) => {
+      setQuestionId(questionId);
+      setQuestionIdInput(questionId);
+    });
+    socket?.emit('join-room', roomId);
+  }, [socket]);
+
+  return (
+    <VStack as="div" style={{ overflowY: 'auto', height: '100%', paddingLeft: '16px', paddingRight: '16px' }}>
+      {/* TODO: something about this box */}
+      <Box
+        width="100%"
+        padding={4}
+        alignSelf="flex-start"
+        _light={{ backgroundColor: 'gray.200' }}
+        _dark={{ backgroundColor: 'gray.700' }}
+        borderRadius={8}
+      >
+        <HStack spacing={4}>
+          <Text>Selected Question ID</Text>
+          <NumberInput
+            size="sm"
+            maxWidth="80px"
+            onChange={(e) => {
+              setQuestionIdInput(parseInt(e, 10));
+            }}
+          >
+            <NumberInputField />
+          </NumberInput>
+          <Button
+            size="sm"
+            onClick={() => {
+              setQuestionId(questionIdInput);
+            }}
+          >
+            Update
+          </Button>
+        </HStack>
+      </Box>
+      {questionId === undefined && (
+        <Text mt={16} size="xl">
+          No question selected yet
+        </Text>
+      )}
+      {questionId !== undefined && <QuestionDetails questionId={questionId} />}
+    </VStack>
+  );
+};
+
+export default CollaborationQuestion;
