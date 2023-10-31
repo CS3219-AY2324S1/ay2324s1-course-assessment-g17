2f1d00d2a11963f5fbc2f898c68e1c6ce4381237
sltsheryl <sltsheryl@gmail.com>
2023-10-31 18:21:17 +0800

    feat: add reset password form (input new password)

---
+++ frontend/src/pages/auth/ResetPassword.tsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { FormControl, FormLabel, Input, Button, Box, useToast } from '@chakra-ui/react';
+import { useNavigate, useLocation } from 'react-router-dom';
+
+const ResetPassword: React.FC = () => {
+  const [password, setPassword] = useState('');
+  const toast = useToast();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const token = searchParams.get('token');
+
+  const handleSubmit = (e: React.FormEvent): void => {
+    e.preventDefault();
+    console.log('HURT');
+    console.log(token);
+
+    axios
+      .post(`http://localhost:8000/reset-password?token=${token}`, {
+        password,
+      })
+      .then(() => {
+        toast({
+          title: 'Password reset successful!',
+          description: 'You can now login with your new password.',
+          duration: 2000,
+          status: 'success',
+          isClosable: true,
+        });
+        navigate('/');
+      })
+      .catch((error) => {
+        toast({
+          title: 'Error resetting password!',
+          description: 'Please try again later.',
+          duration: 2000,
+          status: 'error',
+          isClosable: true,
+        });
+        console.error('Error resetting password:', error);
+      });
+  };
+
+  return (
+    <Box maxW="md" mx="auto" mt={8} p={6} borderWidth={1} borderRadius="lg">
+      <form onSubmit={handleSubmit}>
+        <FormControl id="password" isRequired>
+          <FormLabel>New Password</FormLabel>
+          <Input
+            type="password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+        </FormControl>
+        <Button type="submit" mt={4} colorScheme="teal">
+          Reset Password
+        </Button>
+      </form>
+    </Box>
+  );
+};
+
+export default ResetPassword;
--- frontend/src/pages/routing/UnauthenticatedApp.tsx
+++ frontend/src/pages/routing/UnauthenticatedApp.tsx
@@ -3,6 +3,7 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import LoginForm from '../auth/LoginForm';
 import SignUpForm from '../auth/SignUpForm';
 import ForgotPassword from '../auth/ForgotPassword';
+import ResetPassword from '../auth/ResetPassword';
 
 const UnauthenticatedApp: React.FC = () => {
   return (
@@ -10,6 +11,7 @@ const UnauthenticatedApp: React.FC = () => {
       <Route path="/signup" element={<SignUpForm />} />
       <Route path="/login" element={<LoginForm />} />
       <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/reset-password" element={<ResetPassword />} />
       <Route path="*" element={<Navigate to="/login" />} />
     </Routes>
   );
