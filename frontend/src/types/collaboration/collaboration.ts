fa664cfef9aba296b0c54ec7b347f1ba1cc9f15a
chloelim <chloelim22@gmail.com>
2023-11-07 19:57:05 +0800

    fix: push missing file

---
+++ frontend/src/types/collaboration/collaboration.ts
@@ -0,0 +1,10 @@
+export interface MatchedPairIdsResponse {
+  _id: string;
+  userOne: number;
+  userTwo: number;
+  room_id: string;
+  complexity: string[];
+  categories: string[];
+  question_ids: number[];
+  __v: number;
+}
