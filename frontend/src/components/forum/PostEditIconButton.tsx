4318410cbd4271eca204a0fee98d2dcf07c3c9b3
Justin Lam, juslam19 <e0552250@u.nus.edu>
2023-11-09 01:23:57 +0800

    fix: get tests to shut up

--- frontend/src/components/forum/PostEditIconButton.tsx
+++ frontend/src/components/forum/PostEditIconButton.tsx
@@ -1,32 +1,30 @@
-// import { EditIcon } from '@chakra-ui/icons';
-// import { Tooltip, IconButton } from '@chakra-ui/react';
-// import { useNavigate } from 'react-router-dom';
-// import React from 'react';
+import { EditIcon } from '@chakra-ui/icons';
+import { Tooltip, IconButton } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
+import React from 'react';
 
-// interface PostEditIconButtonProps {
-//   commentId: number;
-//   postId: number;
-// }
+interface PostEditIconButtonProps {
+  postId: number;
+}
 
-// const PostEditIconButton: React.FC<PostEditIconButtonProps> = ({
-//   commentId,
-//   postId,
-// }: PostEditIconButtonProps) => {
-//   const navigate = useNavigate();
+const PostEditIconButton: React.FC<PostEditIconButtonProps> = ({
+  postId,
+}: PostEditIconButtonProps) => {
+  const navigate = useNavigate();
 
-//   return (
-//     <Tooltip label={'Edit Comment'}>
-//       <IconButton
-//         aria-label="Edit Comment"
-//         onClick={() => {
-//           navigate(`/forum/${postId}/${commentId}/edit`);
-//         }}
-//         ml={4}
-//       >
-//         <EditIcon />
-//       </IconButton>
-//     </Tooltip>
-//   );
-// };
+  return (
+    <Tooltip label={'Edit Post'}>
+      <IconButton
+        aria-label="Edit Comment"
+        onClick={() => {
+          navigate(`/forum/${postId}/edit`);
+        }}
+        ml={4}
+      >
+        <EditIcon />
+      </IconButton>
+    </Tooltip>
+  );
+};
 
-// export default PostEditIconButtonProps;
+export default PostEditIconButtonProps;
