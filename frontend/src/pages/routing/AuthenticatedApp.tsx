ba3ec205c9e0922edeaa797b3c150df54b06aecc
Justin Lam, juslam19 <e0552250@u.nus.edu>
2023-11-11 01:43:01 +0800

    removed comments

--- frontend/src/pages/routing/AuthenticatedApp.tsx
+++ frontend/src/pages/routing/AuthenticatedApp.tsx
@@ -8,30 +8,6 @@ import ViewQuestion from '../questions/ViewQuestion';
 import PageNotFound from './NotFound';
 
 const AuthenticatedApp: React.FC = () => {
-  // const location = useLocation();
-  // const authApi = new AuthAPI();
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  // //     try {
-  // //       // Ensure that useRefreshToken is awaited
-  //       await authApi.useRefreshToken();
-
-  // //       // Now you can make additional API calls after refreshing the token
-  // //       const user = await authApi.getCurrentUser();
-  // //       console.log(`Fresh token granted for ${user.username}`);
-
-  // //       // Code to run when the route changes
-  // //       console.log('Page changed:', location.pathname);
-  // //     } catch (error) {
-  // //       console.error('Error:', error);
-  // //     }
-  //   };
-
-  // //   // Call the async function immediately using an IIFE
-  //   fetchData();
-  // }, [location]); // Ensures effect runs when the route changes
-
   return (
     <Routes>
       <Route path="/" element={<Questions />} />
