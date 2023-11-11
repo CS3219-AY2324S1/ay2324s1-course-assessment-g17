e7d55634840f814ebdaaee33ba4197f8f7a71fc9
Justin Lam, juslam19 <e0552250@u.nus.edu>
2023-11-11 22:56:01 +0800

    fix: logout, access and refresh token generation only use role and id, fetch access token when expired whenever page changes

--- frontend/src/api/base.ts
+++ frontend/src/api/base.ts
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import type { AxiosError } from 'axios';
+import AuthAPI from './users/auth';
 
 const client = axios.create({
   baseURL: process.env.REACT_APP_BACKEND_URL,
@@ -12,6 +14,25 @@ export const userServiceClient = axios.create({
 
 export const codeExecutionServiceClient = axios.create({
   baseURL: `https://${process.env.REACT_APP_RAPID_API_HOST}`,
+});
+
+// Add a response interceptor
+client.interceptors.response.use(null, async (error: AxiosError) => {
+  // eslint-disable-next-line @typescript-eslint/prefer-optional-chain
+  if (error != null && error.config != null && error.response != null) {
+    if (error.response.status === 401) {
+      // eslint-disable-next-line no-useless-catch
+      try {
+        const authApi = new AuthAPI();
+        await authApi.useRefreshToken();
+        console.log('Refreshed1');
+        return await axios.request(error.config);
+      } catch (error) {
+        throw error;
+      }
+    }
+  }
+  return error.response;
 });
 
 export default client;
