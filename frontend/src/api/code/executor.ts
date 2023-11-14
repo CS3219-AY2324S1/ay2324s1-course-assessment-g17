61f36506095070a3171c65772639b13e1e264ec4
chloelim <chloelim22@gmail.com>
2023-11-15 03:38:36 +0800

    fix: move code exec api to backend

--- frontend/src/api/code/executor.ts
+++ frontend/src/api/code/executor.ts
@@ -3,31 +3,17 @@ import { codeExecutionServiceClient } from '../base';
 
 export default class ExecutorAPI {
   protected getExecutorUrl(): string {
-    return '/submissions';
+    return '/api/code-execute';
   }
 
   public async submitCode(codeSubmission: ExecutorSubmissionPostData): Promise<string> {
-    const response = await codeExecutionServiceClient.post(this.getExecutorUrl(), codeSubmission, {
-      params: { base64_encoded: 'true', fields: '*' },
-      headers: {
-        'content-type': 'application/json',
-        'Content-Type': 'application/json',
-        'X-RapidAPI-Host': process.env.REACT_APP_RAPID_API_HOST,
-        'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-      },
-    });
-    const token = response.data.token as string;
+    const response = await codeExecutionServiceClient.post(this.getExecutorUrl(), codeSubmission);
+    const token = response.data as string;
     return token;
   }
 
   public async getExecutorSubmissionData(token: string): Promise<ExecutorStatusData> {
-    const response = await codeExecutionServiceClient.get(`${this.getExecutorUrl()}/${token}`, {
-      params: { base64_encoded: 'true', fields: '*' },
-      headers: {
-        'X-RapidAPI-Host': process.env.REACT_APP_RAPID_API_HOST,
-        'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-      },
-    });
+    const response = await codeExecutionServiceClient.get(`${this.getExecutorUrl()}/${token}`);
     const submissionStatus = response.data as ExecutorStatusData;
     return submissionStatus;
   }
